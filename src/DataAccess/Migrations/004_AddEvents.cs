22162b9195613f007093d0aee9c360792ace4057
bdsutton <bdsutton@905cb14f-4d54-0410-abee-95902bd6830d>
2008-08-28 05:19:26 +0000

    added events and facilities

---
+++ src/DataAccess/Migrations/004_AddEvents.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using SubSonic;
+
+namespace CRIneta.DataAccess.Migrations
+{
+    class Migration004_AddEvents : Migration
+    {
+        private const string EVENTS_TABLE_NAME = "Events";
+
+        public override void Up()
+        {
+            TableSchema.Table events = CreateTableWithKey(EVENTS_TABLE_NAME, "Id");
+            events.AddColumn("Title", DbType.String, 255, false);
+            events.AddColumn("StartsAt", DbType.DateTime, 0, false);
+            events.AddColumn("EndsAt", DbType.DateTime, 0, false);
+            events.AddColumn("ImageUrl", DbType.String, 255, false, "''");
+            events.AddLongText("Description", false, "''");
+            events.AddColumn("FacilityId", DbType.Int32, 0, true);
+
+            //add a relationship between Events and Facilities
+            TableSchema.Table facility = GetTable("Facilities");
+            CreateForeignKey(facility.GetColumn("Id"), events.GetColumn("FacilityId"));
+        }
+
+        public override void Down()
+        {
+            TableSchema.Table facility = GetTable("Facilities");
+            TableSchema.Table events = GetTable("Events");
+
+            DropForeignKey(facility.GetColumn("Id"), events.GetColumn("FacilityId"));
+            DropTable(EVENTS_TABLE_NAME);
+            
+        }
+    }
+}
