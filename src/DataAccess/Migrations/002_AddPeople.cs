0019f670897b2fda883bc175ece2bc1e38493e49
bdsutton <bdsutton@905cb14f-4d54-0410-abee-95902bd6830d>
2008-08-28 04:31:52 +0000

    added 2nd migration for people

---
+++ src/DataAccess/Migrations/002_AddPeople.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using SubSonic;
+
+namespace CRIneta.DataAccess.Migrations
+{
+    internal class Migration002_AddPeople : Migration
+    {
+        private const string PEOPLE_TABLE_NAME = "People";
+
+        public override void Up()
+        {
+            TableSchema.Table sponsors = CreateTableWithKey(PEOPLE_TABLE_NAME, "Id");
+            sponsors.AddColumn("FirstName", DbType.String, 255, false);
+            sponsors.AddColumn("LastName", DbType.String, 255, false);
+            sponsors.AddColumn("ImageUrl", DbType.String, 255, false, "''");
+            sponsors.AddLongText("Bio", false, "''");
+            sponsors.AddColumn("City", DbType.String, 50, false, "''");
+            sponsors.AddColumn("Region", DbType.String, 50, false, "''");
+        }
+
+        public override void Down()
+        {
+            DropTable(PEOPLE_TABLE_NAME);
+        }
+    }
+}
--- src/Website/Controllers/Controller.cs
+++ src/Website/Controllers/Controller.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Mvc;
 
 namespace CRIneta.Website.Controllers
 {
     public abstract class Controller : System.Web.Mvc.Controller
     {
-        protected override void OnActionExecuting(System.Web.Mvc.ActionExecutingContext filterContext)
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            
         }
 
-        protected override void OnActionExecuted(System.Web.Mvc.ActionExecutedContext filterContext)
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
         {
-            
         }
-
     }
 }
