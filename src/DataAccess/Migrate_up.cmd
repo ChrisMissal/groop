4b9f4f3da6128032c53e660f26c9f5123ab67155
bdsutton <bdsutton@905cb14f-4d54-0410-abee-95902bd6830d>
2008-08-28 18:32:11 +0000

    modified migrations to use meetings instead of events

--- src/DataAccess/Migrate_up.cmd
+++ src/DataAccess/Migrate_up.cmd
@@ -1,8 +1,8 @@
 Rem To run this file successfully make sure you have a database called CRIneta
 Rem on you your localhost\sqlexpress instance that you can read/modify with integrated security
 
-..\..\bin\subsonic\2.1\sonic migrate /version 0
+..\..\lib\subsonic\2.1\sonic migrate /version 0
 
-..\..\bin\subsonic\2.1\sonic migrate
+..\..\lib\subsonic\2.1\sonic migrate
 
 pause
---
+++ src/DataAccess/Migrations/004_AddMeetings.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using SubSonic;
+
+namespace CRIneta.DataAccess.Migrations
+{
+    class Migration004_AddMeetings : Migration
+    {
+        private const string MEETINGS_TABLE_NAME = "Meetings";
+        private const string FACILITIES_TABLE_NAME = "Facilities";
+
+        public override void Up()
+        {
+            TableSchema.Table meetings = CreateTableWithKey(MEETINGS_TABLE_NAME, "Id");
+            meetings.AddColumn("Title", DbType.String, 255, false);
+            meetings.AddColumn("StartsAt", DbType.DateTime, 0, false);
+            meetings.AddColumn("EndsAt", DbType.DateTime, 0, false);
+            meetings.AddColumn("ImageUrl", DbType.String, 255, false, "''");
+            meetings.AddLongText("Description", false, "''");
+            meetings.AddColumn("FacilityId", DbType.Int32, 0, true);
+
+            //add a relationship between Meetings and Facilities
+            TableSchema.Table facility = GetTable(FACILITIES_TABLE_NAME);
+            CreateForeignKey(facility.GetColumn("Id"), meetings.GetColumn("FacilityId"));
+        }
+
+        public override void Down()
+        {
+            TableSchema.Table facility = GetTable(FACILITIES_TABLE_NAME);
+            TableSchema.Table meetings = GetTable(MEETINGS_TABLE_NAME);
+
+            DropForeignKey(facility.GetColumn("Id"), meetings.GetColumn("FacilityId"));
+            DropTable(MEETINGS_TABLE_NAME);
+
+        }
+    }
+}
